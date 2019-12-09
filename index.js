4018af33d4a4812be01f4c809df9468126a5a4e4
roblotter <roblotter1@gmail.com>
2019-12-09 17:15:46 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-const core = require('@actions/core');
-const wait = require('./wait');
+const core = require("@actions/core");
 
+const { GitHub, context } = require("@actions/github");
 
 // most @actions toolkit packages have async methods
 async function run() {
-  try { 
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+  try {
+    const github = new GitHub(process.env.GITHUB_TOKEN);
 
-    core.debug((new Date()).toTimeString())
-    wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
+    const { ref } = context;
+    const commit = await github.git.getCommit(ref);
 
-    core.setOutput('time', new Date().toTimeString());
-  } 
-  catch (error) {
+    core.debug(`commit: JSON.stringify(${commit})`);
+  } catch (error) {
     core.setFailed(error.message);
   }
 }
 
-run()
+run();
